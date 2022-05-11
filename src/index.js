3b8f7ffba690ed08704c19bd0e384e59c40bf4ce
nori1jz <86910924+nori1jz@users.noreply.github.com>
2022-05-11 12:58:35 +0900

    デフォルト値

--- src/index.js
+++ src/index.js
@@ -99,12 +99,18 @@
 // const message2 = `名前は${name}です。年齢は${age}歳です。`;
 // console.log(message2);
 
-const myProfile = ["じゃけえ", 28];
+// const myProfile = ["じゃけえ", 28];
 
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-console.log(message3);
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
 
-//配列は順番が大事！
-const [name, age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message4);
+// //配列は順番が大事！
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+sayHello("じゃけえ");
